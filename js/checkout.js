4232c046fac679eaa6c0899fc10711c3bc69640d
Abhishek <choudharyabhishekk@gmail.com>
2023-12-12 00:24:17 -0500

    images

--- js/checkout.js
+++ js/checkout.js
@@ -1,94 +1,94 @@
 $(function () {
-    $.validator.setDefaults({
-        submitHandler: function () {
-            $("#submit-dialog").dialog("open");
-        }
-    });
+  $.validator.setDefaults({
+    submitHandler: function () {
+      $("#submit-dialog").dialog("open");
+    },
+  });
 
-    $("#province").selectmenu();
+  $("#province").selectmenu();
 
-    $("#submit-dialog").dialog({
-        autoOpen: false,
-        modal: true,
-        buttons: {
-            "Continue shopping": function () {
-                $(this).dialog("close");
-                window.location.href = "./product_listings.html";
-            }
-        }
-    });
-    // validate shipping-form form and submit
-    $("#shipping-form").validate({
-        rules: {
-            firstname: {
-                required: true,
-                minlength: 2
-            },
-            lastname: {
-                required: true,
-                minlength: 2 
-            },
-            address: {
-                required: true,
-                minlength: 5
-            },
-            city: {
-                required: true,
-                minlength: 2,
-            },
-            "postal-code": {
-                required: true,
-                minlength: 6,
-                maxlength: 6,
-            },
-            phone: {
-                required: true,
-                phoneUS: true
-            },
-            email: {
-                required: true,
-                email: true
-            },
-            "order-notes": {
-                maxlength: 200,
-            }
-        },
-        messages: {
-            firstname: {
-                required: "Please enter your firstname",
-                minlength: "Your firstname must consist of at least 2 characters"
-            },
-            lastname: {
-                required: "Please enter your lastname",
-                minlength: "Your lastname must consist of at least 2 characters"
-            },
-            address: {
-                required: "Please provide a address",
-                minlength: "Your address must be at least 5 characters long"
-            },
-            city: {
-                required: "Please provide a city",
-                minlength: "Your city must be at least 2 characters long"
-            },
-            "postal-code": {
-                required: "Please provide a postal code",
-                minlength: "Your postal code must be 6 characters long (exclude space)",
-                maxlength: "Your postal code must be 6 characters long (exclude space)"
-            },
-            phone: {
-                required: "Please provide your phone",
-                phoneUS: "Your phone must be a valid phone number"
-            },
+  $("#submit-dialog").dialog({
+    autoOpen: false,
+    modal: true,
+    buttons: {
+      "Continue shopping": function () {
+        $(this).dialog("close");
+        window.location.href = "./products.html";
+      },
+    },
+  });
+  // validate shipping-form form and submit
+  $("#shipping-form").validate({
+    rules: {
+      firstname: {
+        required: true,
+        minlength: 2,
+      },
+      lastname: {
+        required: true,
+        minlength: 2,
+      },
+      address: {
+        required: true,
+        minlength: 5,
+      },
+      city: {
+        required: true,
+        minlength: 2,
+      },
+      "postal-code": {
+        required: true,
+        minlength: 6,
+        maxlength: 6,
+      },
+      phone: {
+        required: true,
+        phoneUS: true,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+      "order-notes": {
+        maxlength: 200,
+      },
+    },
+    messages: {
+      firstname: {
+        required: "Please enter your firstname",
+        minlength: "Your firstname must consist of at least 2 characters",
+      },
+      lastname: {
+        required: "Please enter your lastname",
+        minlength: "Your lastname must consist of at least 2 characters",
+      },
+      address: {
+        required: "Please provide a address",
+        minlength: "Your address must be at least 5 characters long",
+      },
+      city: {
+        required: "Please provide a city",
+        minlength: "Your city must be at least 2 characters long",
+      },
+      "postal-code": {
+        required: "Please provide a postal code",
+        minlength: "Your postal code must be 6 characters long (exclude space)",
+        maxlength: "Your postal code must be 6 characters long (exclude space)",
+      },
+      phone: {
+        required: "Please provide your phone",
+        phoneUS: "Your phone must be a valid phone number",
+      },
 
-            email: {
-                required: "Please enter a valid email address",
-                email: "Your email must be a valid email address"
-            },
-            "order-notes": {
-                maxlength: "Your order notes must be less than 200 characters long"
-            },
-        }
-    });
+      email: {
+        required: "Please enter a valid email address",
+        email: "Your email must be a valid email address",
+      },
+      "order-notes": {
+        maxlength: "Your order notes must be less than 200 characters long",
+      },
+    },
+  });
 
-    $("#firstname").focus();
+  $("#firstname").focus();
 });
--- js/products.js
+++ js/products.js
@@ -1,24 +1,29 @@
 $(document).ready(function () {
-  const productListDiv = $("#product-list");
-
+  var startIndex = 0;
+  var endIndex = 9;
   // Loop through the products and create HTML elements to display them
-  products.forEach((product) => {
-    const productDiv = $('<div class="product">');
-    productDiv.html(`
-    <img src="${product.image}" alt="${[product.name]} id=${product.id}">
-     <div class="product-content">
+  const displayProducts = (startIndex, endIndex) => {
+    const productListDiv = $("#product-list");
+    const productsToShow = products.slice(startIndex, endIndex);
+    productsToShow.forEach((product) => {
+      const productDiv = $(`<div class="product" id=${product.id}>`);
+      productDiv.html(`
+    <img src="${product.image}" alt="${[product.name]}" id=${product.id}>
+     <div class="product-content" id=${product.id}>
       <div class="product-title">${product.name}</div>
      <div class="product-price">$${product.price}</div>
      <button class="btn-add-to-cart" id=${product.id}>Add to Cart</button>
    </div>
                 </div>
     `);
-    productListDiv.append(productDiv);
-  });
+      productListDiv.append(productDiv);
+    });
+  };
+  displayProducts(startIndex, endIndex);
 
-  $(".product img").click((e) => {
-    e.preventDefault();
-    console.log($(this).attr("id"));
+  $(".product img").click(function (e) {
+    var selectedProductID = $(this).attr("id");
+    window.location.href = `product-page.html?id=${selectedProductID}`;
   });
 
   // add to cart button event listener
@@ -27,7 +32,6 @@ $(document).ready(function () {
     if ($(this).text() == "Proceed to Checkout") {
       window.location.href = "cart.html";
     } else {
-      console.log($(this).attr("id"));
       var cartData = $(this).attr("id");
       localStorage.setItem(
         "cart",
@@ -38,7 +42,6 @@ $(document).ready(function () {
       );
 
       $(this).css("background-color", "#a33115");
-
       $(this).text("Proceed to Checkout");
     }
     //  to fetch data in carts page use the follow code
@@ -46,4 +49,28 @@ $(document).ready(function () {
     //   console.log(item.productName, item.productPrice);
     // });
   });
+
+  $("#loadProducts").click(() => {
+    startIndex = endIndex;
+    endIndex = products.length;
+    displayProducts(startIndex, endIndex);
+  });
+
+  const productPageDetails = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const productId = urlParams.get("id");
+
+    const productDetails = products.find((product) => product.id == productId);
+
+    // Update the HTML elements on the product page
+    $("#product_image").attr("src", productDetails.image);
+    $("#product_title").text(productDetails.name);
+    $("#product_price").text(`$${productDetails.price}`);
+    $("#product_description").text(productDetails.description);
+  };
+
+  // Call the productPageDetails function on product page load
+  if (window.location.pathname.includes("product-page.html")) {
+    productPageDetails();
+  }
 });
